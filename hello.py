4f0b38f641cd100f4ee89b63061fcf540442dde7
manu mathew <manmathew@cs.stonybrook.edu>
2018-05-06 16:42:40 -0400

    check

--- hello.py
+++ hello.py
@@ -17,7 +17,6 @@ def stat():
 		return render_template('analytics.html')
 
 if __name__ == "__main__":
-	#app.run()
 	#app.run(host='0.0.0.0')
 	app.run(host="0.0.0.0", port=5000, threaded=True)
 	#app.run(host='172.24.18.62')
